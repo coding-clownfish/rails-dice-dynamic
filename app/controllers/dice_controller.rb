628863781613c7270b9b396cb335f5256044ee9d
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-04-24 22:48:55 +0000

    to de-bug

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,14 +4,14 @@ class DiceController < ApplicationController
 
     @num_dice = params.fetch("number_of_dice").to_i
     
-
     @rolls=[]
 
     @num_dice.times do
       die = rand(1..6)
       
-      @rolls.push(die)
-    end
+    @rolls.push(die)
+    
+  end
 
     render({:template => "dice_templates/2d6"})
   end
