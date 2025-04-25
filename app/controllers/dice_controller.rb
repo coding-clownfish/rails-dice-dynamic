b29875ecb91c4379151d8f4f43d066791729dd7c
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-04-25 00:44:03 +0000

    before homepage

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,11 +3,12 @@ class DiceController < ApplicationController
   def two_six
 
     @num_dice = params.fetch("number_of_dice").to_i
+    @sides_dice = params.fetch("sides_of_dice").to_i
     
     @rolls=[]
 
     @num_dice.times do
-      die = rand(1..6)
+      die = rand(1..@sides_dice)
       
     @rolls.push(die)
     
